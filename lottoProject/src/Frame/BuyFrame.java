b59adb798b896a83c5d0f85b5408cc5bff38b446
sara <sarazzang05@gmail.com>
2023-05-26 14:43:47 +0900

    메인 메뉴들만 연결시켜 봤습니다.

--- lottoProject/src/Frame/BuyFrame.java
+++ lottoProject/src/Frame/BuyFrame.java
@@ -1,6 +1,5 @@
 package Frame;
 
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -17,25 +16,30 @@ import javax.swing.border.EmptyBorder;
 
 import lottoProject.PaymentTicketManager;
 
+/**
+ * @author 이사라
+ * @Class 로또 구매 하기
+ */
 public class BuyFrame extends JFrame {
 
 	PaymentTicketManager paymentTicketManager;
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					BuyFrame frame = new BuyFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					BuyFrame frame = new BuyFrame();
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	private JPanel contentPane;
 	ArrayList<JButton> buttons;
-	
+
 	private int count;
 	Set<Integer> numbers;
 	int price;
@@ -48,6 +52,7 @@ public class BuyFrame extends JFrame {
 
 	private void 결제초기화버튼액션리스너추가() {
 	}
+
 	private void 결제초기화버튼인덱스리턴() {
 	}
 
@@ -61,6 +66,7 @@ public class BuyFrame extends JFrame {
 		};
 
 	}
+
 	public BuyFrame() {
 		paymentTicketManager = new PaymentTicketManager();
 		결제초기화버튼액션리스너추가();
@@ -72,39 +78,39 @@ public class BuyFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel pnlLottoNum = new JPanel();
 		pnlLottoNum.setBounds(55, 78, 248, 249);
 		contentPane.add(pnlLottoNum);
-		pnlLottoNum.setLayout(new GridLayout(9,5));
+		pnlLottoNum.setLayout(new GridLayout(9, 5));
 
 		// 45개의 로또 번호 버튼배열입니다
-	    buttons = new ArrayList<>();
-	 
-        for (int i = 1; i <= 45; i++) {
-            JButton button = new JButton(Integer.toString(i));
-            button.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                	if(count < 6) {
-                	// 버튼 정보 보내줄 예정
-                	// numbers.add(Integer.parseInt(button.getText())); 
-                    button.setEnabled(false);
-                    	if(count == 6) {
-                    		button.setEnabled(true);
-                    	}
-                    count++;
-                	}
-                }
-            });
-            buttons.add(button);
-            pnlLottoNum.add(button);
-        }
-	    
+		buttons = new ArrayList<>();
+
+		for (int i = 1; i <= 45; i++) {
+			JButton button = new JButton(Integer.toString(i));
+			button.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if (count < 6) {
+						// 버튼 정보 보내줄 예정
+						// numbers.add(Integer.parseInt(button.getText()));
+						button.setEnabled(false);
+						if (count == 6) {
+							button.setEnabled(true);
+						}
+						count++;
+					}
+				}
+			});
+			buttons.add(button);
+			pnlLottoNum.add(button);
+		}
+
 		JPanel pnlChoice = new JPanel();
 		pnlChoice.setBounds(339, 88, 327, 270);
 		contentPane.add(pnlChoice);
-		
-		for(int i = 0; i < 5; i++) {
+
+		for (int i = 0; i < 5; i++) {
 			JPanel choice = new JPanel();
 			pnlChoice.add(choice);
 			JLabel isAuto = new JLabel("자동여부");
@@ -116,15 +122,22 @@ public class BuyFrame extends JFrame {
 			choice.add(btnReset);
 		} // 기본 값
 		pnlChoice.setLayout(null);
-		
+
 		JPanel pnlSouth = new JPanel();
 		pnlSouth.setBounds(51, 5, 225, 43);
 		pnlChoice.add(pnlSouth);
-		
+
 		JButton btnPayment = new JButton("결제하기");
+		btnPayment.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				PaymentCheckFrame checkPay = new PaymentCheckFrame();
+				checkPay.setVisible(true);
+			}
+		});
 		pnlSouth.add(btnPayment);
 		btnPayment.setFont(new Font("굴림", Font.PLAIN, 20));
-		
+
 		btnNewButton = new JButton("초기화");
 		btnNewButton.setBounds(218, 58, 97, 23);
 		pnlChoice.add(btnNewButton);
@@ -153,55 +166,64 @@ public class BuyFrame extends JFrame {
 		});
 		btnResetAll.setBounds(218, 223, 97, 23);
 		pnlChoice.add(btnResetAll);
-		
+
 		JLabel lblTotal = new JLabel("***** 원");
 		lblTotal.setFont(new Font("굴림", Font.PLAIN, 22));
 		lblTotal.setBounds(365, 39, 95, 23);
 		contentPane.add(lblTotal);
-		
+
 		JLabel lblMyMoney = new JLabel("예치금 : ***** 원");
 		lblMyMoney.setFont(new Font("굴림", Font.PLAIN, 22));
 		lblMyMoney.setBounds(62, 39, 188, 23);
 		contentPane.add(lblMyMoney);
-		
+
 		JButton btnReload = new JButton("충전");
 		btnReload.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				DepositFrame deposit = new DepositFrame();
+				deposit.setVisible(true);
 			}
 		});
 		btnReload.setBounds(248, 39, 62, 23);
 		contentPane.add(btnReload);
-		
+
 		JButton btnMain = new JButton("메인으로");
+		btnMain.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				dispose();
+			}
+		});
 		btnMain.setBounds(544, 42, 97, 23);
 		contentPane.add(btnMain);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(22, 337, 303, 33);
 		contentPane.add(panel);
-		
+
 		JButton btnAutoPlus = new JButton("자동버튼");
 		btnAutoPlus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				  if (count < 6) {
-	                    // 선택하지 않은 번호 중에서 랜덤하게 선택
-	                    Random random = new Random();
-	                    int randomNum = random.nextInt(45)+1;
-	                    while (count < 6)
-	                    	//buttons(randomNum).setEnabled(false);
-	                        count++;
-				  }
+				if (count < 6) {
+					// 선택하지 않은 번호 중에서 랜덤하게 선택
+					Random random = new Random();
+					int randomNum = random.nextInt(45) + 1;
+					while (count < 6)
+						// buttons(randomNum).setEnabled(false);
+						count++;
+				}
 			}
 		});
 		panel.add(btnAutoPlus);
-		
+
 		JButton btnResetList = new JButton("선택 초기화");
 		btnResetList.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		panel.add(btnResetList);
-		
+
 		JButton btnPlus = new JButton("등록");
 		panel.add(btnPlus);
 		btnPlus.addActionListener(new ActionListener() {
@@ -209,7 +231,5 @@ public class BuyFrame extends JFrame {
 			}
 		});
 	}
-
-
 
 }
--- lottoProject/src/Frame/MainFrame.java
+++ lottoProject/src/Frame/MainFrame.java
@@ -1,15 +1,14 @@
 package Frame;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class MainFrame extends JFrame {
 
@@ -42,38 +41,59 @@ public class MainFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 434, 261);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("New label");
 		lblNewLabel.setBounds(12, 10, 410, 129);
 		panel.add(lblNewLabel);
-		
+
 		JButton btnNewButton = new JButton("구매하기");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				BuyFrame buy = new BuyFrame();
+				buy.setVisible(true);
+				setVisible(false);
+			}
+		});
 		btnNewButton.setBounds(90, 149, 97, 33);
 		panel.add(btnNewButton);
-		
+
 		JButton btnNewButton_1 = new JButton("구매내역");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				BuyList buyList = new BuyList();
+				buyList.setVisible(true);
+				setVisible(false);
 			}
 		});
 		btnNewButton_1.setBounds(249, 149, 97, 33);
 		panel.add(btnNewButton_1);
-		
+
 		JButton btnNewButton_2 = new JButton("추첨");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				ShootNumImage shootNumImage = new ShootNumImage();
+				shootNumImage.setVisible(true);
+				setVisible(false);
 			}
 		});
 		btnNewButton_2.setBounds(90, 204, 97, 33);
 		panel.add(btnNewButton_2);
-		
+
 		JButton btnNewButton_3 = new JButton("당첨 확인");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				WinningDetails winningDetails = new WinningDetails();
+				winningDetails.setVisible(true);
+				setVisible(false);
+			}
+		});
 		btnNewButton_3.setBounds(249, 204, 97, 33);
 		panel.add(btnNewButton_3);
+//		EndCheckFrame
 	}
 }
