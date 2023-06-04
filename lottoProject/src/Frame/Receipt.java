7bc5508456c386cd087d8b2753ae5f8b3dcae9e2
YimFun9 <catbaby147@gmail.com>
2023-06-04 14:22:34 +0900

    Merge branch 'master' of https://github.com/YimFun9/IdeaRepository.git

--- lottoProject/src/Frame/Receipt.java
+++ lottoProject/src/Frame/Receipt.java
@@ -1,7 +1,9 @@
 package Frame;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,6 +13,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -28,15 +31,18 @@ public class Receipt extends JDialog implements MouseListener {
 	private JPanel[] choices;
 	private JLabel[] isAutos;
 	private JLabel[] numbers;
-	private JLabel backLabel;
+	private JLabel backgroundLabel;
+	private JLabel centerLabel;
 	
 	
 	public Receipt(LottoPaper lottoPaper) {
+		
+		
 
 		setModal(true);
 
 		System.out.println(lottoPaper.getCount().size()); // 카운트의 사이즈;
-		setBounds(100, 100, 400, 10);
+		setBounds(100, 100, 400, 700); //10 이었음
 		contentPane = new JPanel();
 		contentPane.setBackground(SystemColor.window);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -45,62 +51,38 @@ public class Receipt extends JDialog implements MouseListener {
 		
 		addMouseListener(this);
 		
+		backgroundLabel = new JLabel("");
+		backgroundLabel.setBounds(0, 0, 384, 661);
+		backgroundLabel.setIcon(new ImageIcon(BuyList.class.getResource("/imagepackage/배경라벨이미지.png")));
+		backgroundLabel.setBounds(0, 0, 1000, 600);
+		contentPane.add(backgroundLabel);
+		
 		int size = lottoPaper.getCount().size();
-
-		JLabel lblNewLabel = new JLabel("LOTTO");
-		lblNewLabel.setFont(new Font("맑은 고딕", Font.BOLD, 31));
-		lblNewLabel.setBounds(77, 51, 111, 85);
-		contentPane.add(lblNewLabel);
-
-		JLabel lblNewLabel_1 = new JLabel("LOTTO");
-		lblNewLabel_1.setForeground(Color.PINK);
-		lblNewLabel_1.setFont(new Font("맑은 고딕", Font.BOLD, 30));
-		lblNewLabel_1.setBounds(37, 10, 97, 76);
-		contentPane.add(lblNewLabel_1);
-
-		JLabel lblNewLabel_2 = new JLabel("6 / 45");
-		lblNewLabel_2.setFont(new Font("맑은 고딕", Font.BOLD, 18));
-		lblNewLabel_2.setBounds(194, 91, 57, 15);
-		contentPane.add(lblNewLabel_2);
-
-		JLabel lblNewLabel_2_1 = new JLabel("6 / 45");
-		lblNewLabel_2_1.setForeground(Color.PINK);
-		lblNewLabel_2_1.setFont(new Font("맑은 고딕", Font.BOLD, 18));
-		lblNewLabel_2_1.setBounds(146, 49, 57, 15);
-		contentPane.add(lblNewLabel_2_1);
 
 		JLabel lblNewLabel_3 = new JLabel("발 행 일 :");
 		lblNewLabel_3.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		lblNewLabel_3.setForeground(new Color(250, 250, 210));
 		lblNewLabel_3.setBounds(37, 160, 83, 15);
-		contentPane.add(lblNewLabel_3);
+		backgroundLabel.add(lblNewLabel_3);
 
 		JLabel lblNewLabel_3_1 = new JLabel("추 첨 일 :");
 		lblNewLabel_3_1.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		lblNewLabel_3_1.setForeground(new Color(250, 250, 210));
 		lblNewLabel_3_1.setBounds(37, 185, 74, 15);
-		contentPane.add(lblNewLabel_3_1);
+		backgroundLabel.add(lblNewLabel_3_1);
 
 		JLabel lblNewLabel_3_2 = new JLabel("금액");
 		lblNewLabel_3_2.setFont(new Font("맑은 고딕", Font.BOLD, 18));
+		lblNewLabel_3_2.setForeground(new Color(250, 250, 210));
 		lblNewLabel_3_2.setBounds(51, 416, 83, 37);
-		contentPane.add(lblNewLabel_3_2);
+		backgroundLabel.add(lblNewLabel_3_2);
 
 		JLabel lblNewLabel_3_2_1 = new JLabel("\\ 5,000");
 		lblNewLabel_3_2_1.setText("\\ " + size + ",000");
 		lblNewLabel_3_2_1.setFont(new Font("맑은 고딕", Font.BOLD, 18));
+		lblNewLabel_3_2_1.setForeground(new Color(250, 250, 210));
 		lblNewLabel_3_2_1.setBounds(277, 416, 83, 37);
-		contentPane.add(lblNewLabel_3_2_1);
-
-		JLabel lblNewLabel_1_1 = new JLabel("LOTTO");
-		lblNewLabel_1_1.setForeground(Color.LIGHT_GRAY);
-		lblNewLabel_1_1.setFont(new Font("맑은 고딕", Font.BOLD, 30));
-		lblNewLabel_1_1.setBounds(37, 433, 97, 76);
-		contentPane.add(lblNewLabel_1_1);
-
-		JLabel lblNewLabel_2_1_1 = new JLabel("6 / 45");
-		lblNewLabel_2_1_1.setForeground(Color.LIGHT_GRAY);
-		lblNewLabel_2_1_1.setFont(new Font("맑은 고딕", Font.BOLD, 18));
-		lblNewLabel_2_1_1.setBounds(146, 472, 57, 15);
-		contentPane.add(lblNewLabel_2_1_1);
+		backgroundLabel.add(lblNewLabel_3_2_1);
 
 		JLabel lblNewLabel_5 = new JLabel("2023 / 06 / 07 (수)");
 		LocalDate date = LocalDate.now();
@@ -111,19 +93,33 @@ public class Receipt extends JDialog implements MouseListener {
 		String formattedTime = time.format(formatter);
 		lblNewLabel_5.setText(date.toString() + " " + formattedTime);
 		lblNewLabel_5.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		lblNewLabel_5.setForeground(new Color(250, 250, 210));
 		lblNewLabel_5.setBounds(114, 162, 160, 15);
-		contentPane.add(lblNewLabel_5);
+		backgroundLabel.add(lblNewLabel_5);
 
 		JLabel lblNewLabel_5_1 = new JLabel("2023-06-10 21:00:00");
 		lblNewLabel_5_1.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		lblNewLabel_5_1.setForeground(new Color(250, 250, 210));
 		lblNewLabel_5_1.setBounds(114, 185, 160, 15);
-		contentPane.add(lblNewLabel_5_1);
+		backgroundLabel.add(lblNewLabel_5_1);
 
 		JPanel panel = new JPanel();
 		panel.setBackground(SystemColor.window);
 		panel.setBounds(67, 224, 267, 173);
-		contentPane.add(panel);
+		backgroundLabel.add(panel);
 
+		centerLabel = new JLabel();
+		centerLabel.setBounds(42, 478, 237, 173);
+		ImageIcon centerIcon = new ImageIcon(BuyList.class.getResource("/imagepackage/영수증안쪽.png"));
+		Image centerImg = centerIcon.getImage();
+		Image updateCenterImage = centerImg.getScaledInstance(centerLabel.getWidth(), centerLabel.getHeight(),
+				Image.SCALE_SMOOTH);
+		ImageIcon updateCenterIcon = new ImageIcon(updateCenterImage);
+		centerLabel.setIcon(updateCenterIcon);
+		centerLabel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panel.add(centerLabel);
+		
+		
 		choices = new JPanel[size];
 		isAutos = new JLabel[size];
 		numbers = new JLabel[size];
@@ -132,16 +128,18 @@ public class Receipt extends JDialog implements MouseListener {
 			choices[i] = new JPanel();
 			isAutos[i] = new JLabel("");
 			numbers[i] = new JLabel("");
-
-			choices[i].setBackground(Color.WHITE);
+			choices[i].setOpaque(false);
+			//choices[i].setBackground(Color.WHITE);
 		}
 
 		for (int i = 0; i < size; i++) {
 			choices[i].add(isAutos[i]);
 			choices[i].add(numbers[i]);
 			isAutos[i].setFont(new Font("맑은 고딕", Font.PLAIN, 15));
+			isAutos[i].setForeground(new Color(250, 250, 210));
 			numbers[i].setFont(new Font("맑은 고딕", Font.PLAIN, 15));
-			panel.add(choices[i]);
+			numbers[i].setForeground(new Color(250, 250, 210));
+			centerLabel.add(choices[i]);
 		}
 
 		for (int i = 0; i < size; i++) {
@@ -155,13 +153,22 @@ public class Receipt extends JDialog implements MouseListener {
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(SystemColor.activeCaption);
 		panel_1.setBounds(37, 219, 323, 4);
-		contentPane.add(panel_1);
+		backgroundLabel.add(panel_1);
 
 		JPanel panel_1_1 = new JPanel();
 		panel_1_1.setBackground(SystemColor.activeCaption);
 		panel_1_1.setBounds(37, 413, 323, 4);
-		contentPane.add(panel_1_1);
-
+		backgroundLabel.add(panel_1_1);
+		
+		JLabel logoLabel = new JLabel("");
+		logoLabel.setBounds(67, 20, 267, 112);
+		backgroundLabel.add(logoLabel);
+		ImageIcon logoIcon = new ImageIcon(BuyList.class.getResource("/imagepackage/리그오브로또.png"));
+		Image img = logoIcon.getImage();
+		Image updateImage = img.getScaledInstance(logoLabel.getWidth(), logoLabel.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon updateLogoIcon = new ImageIcon(updateImage);
+		logoLabel.setIcon(updateLogoIcon);
+		
 		// 아래로 내려가는 애니메이션 속도 설정 (밀리초 단위)
 		animationSpeed = 10;
 
